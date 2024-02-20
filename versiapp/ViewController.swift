de8745e86557cf9f61098bd3cf8dc88ee9162635
Leonel Meque <leonel.meque@glovoapp.com>
2024-02-20 23:39:55 +0100

    Initial Commit

---
+++ versiapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  versiapp
+//
+//  Created by Leonel Meque on 20/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
